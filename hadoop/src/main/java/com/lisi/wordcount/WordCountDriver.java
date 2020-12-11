a750fcb3d7b2bcc6e3abf180f6787796f3a5e0ac
lisi <1786678583@qq.com>
2020-12-11 10:36:08 +0800

    修改100

--- hadoop/src/main/java/com/lisi/wordcount/WordCountDriver.java
+++ hadoop/src/main/java/com/lisi/wordcount/WordCountDriver.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 //555
 //666
 //000000000
+//100
 @SuppressWarnings("all")
 public class WordCountDriver {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
